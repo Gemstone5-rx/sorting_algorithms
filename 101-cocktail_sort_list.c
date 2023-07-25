6b45ae341eb45c22f7e5b64292eb63d7c01921d1
Gemstone5-rx <pearludoka2000@gmail.com>
2023-07-25 13:27:51 -0700

    updated

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,94 +1,91 @@
 #include "sort.h"
+/**
+ * swap1 - swaps nodes from left to right
+ * @list: pointer to list
+ * @head: pointer to head node
+ * @aux: auxiliar pointer
+ * Return: no return
+ */
+void swap1(listint_t **list, listint_t *head, listint_t *aux)
+{
+	if (head->prev)
+		head->prev->next = aux;
+	else
+		*list = aux;
+	if (aux->next)
+		aux->next->prev = head;
+	head->next = aux->next;
+	aux->prev = head->prev;
+	aux->next = head;
+	head->prev = aux;
+	print_list(*list);
+
+}
+/**
+ * swap2 - swaps nodes from right to left
+ * @list: pointer to list
+ * @head: pointer to head node
+ * @aux: auxiliar pointer
+ * Return: no return
+ */
+void swap2(listint_t **list, listint_t *head, listint_t *aux)
+{
+	aux = head->prev;
+	aux->next->prev = aux->prev;
+	if (aux->prev)
+		aux->prev->next = aux->next;
+	else
+		*list = aux->next;
+	aux->prev = aux->next;
+	aux->next = aux->next->next;
+	aux->prev->next = aux;
+	if (aux->next)
+		aux->next->prev = aux;
+	print_list(*list);
+}
 
 /**
- * partition - function that partitions an array.
- *
- * @array: pointer to array of integers to be partitioned.
- * @size: number of integers in array.
- * @beg: start of array.
- * @end: end of array.
- *
- * Return: The partion index.
- */
-int partition(int *array, int beg, int end, int size)
+ * cocktail_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Cocktail sort ailgorithm
+ * @list: pointer to the list head
+ * Return: no return
+ **/
+void cocktail_sort_list(listint_t **list)
 {
-	/* select the right most element as pivot */
-	int pivot = array[end];
+	listint_t *head, *aux;
+	int flag = 1;
 
-	/* set partition index as start initially */
-	int partition_index = beg;
-
-	int i, temp;
-
-	for (i = beg; i < end; i++)
+	if (list)
 	{
-		if (array[i] <= pivot)
+		head = *list;
+		while (flag != 0)
 		{
-			if (partition_index != i)
+			flag = 0;
+			while (head->next)
 			{
-				temp = array[partition_index];
-				array[partition_index] = array[i];
-				array[i] = temp;
-				print_array(array, size);
+				if (head->n > head->next->n)
+				{
+					aux = head->next;
+					swap1(list, head, aux);
+					flag = 1;
+				}
+				else
+					head = head->next;
 			}
-			partition_index++;
+			if (flag == 0)
+				break;
+			flag = 0;
+			while (head->prev)
+			{
+				if (head->prev->n > head->n)
+				{
+					swap2(list, head, aux);
+					flag = 1;
+				}
+				else
+					head = head->prev;
+			}
+
 		}
 	}
-	if (partition_index != end)
-	{
-		/* swap pivot with element at partion index */
-		temp = array[partition_index];
-		array[partition_index] = array[end];
-		array[end] = temp;
-		print_array(array, size);
-	}
-	return (partition_index);
-}
-
-/**
- * quickSort - function that sorts an array of integers using recursion.
- *
- * @array: pointer to array of integers to be sorted.
- * @size: number of integers in array.
- * @beg: start of array.
- * @end: end of array.
- *
- * Return: No return.
- */
-void quickSort(int *array, int beg, int end, int size)
-{
-	int partion_index;
-
-	if (beg < end)
-	{
-		partion_index = partition(array, beg, end, size);
-
-		/* recursive call on the left of pivot */
-		quickSort(array, beg, partion_index - 1, size);
-
-		/* recursive call on the right of pivot */
-		quickSort(array, partion_index + 1, end, size);
-	}
-}
-
-/**
- * quick_sort - sorts an array of integers in ascending order using the,
- * Quick sort sort algorithm.
- *
- * @array: pointer to array of integers to be sorted.
- * @size: number of integers in array.
- *
- * Implement the Lomuto partition scheme.
- * The pivot should always be the last element of the partition being sorted.
- * Print the array after each time you swap two elements.
- *
- * Return: No return.
- */
-void quick_sort(int *array, size_t size)
-{
-	/* An array does not need to be sorted if its size is less than 2 */
-	if (!array || size < 2)
-		return;
-
-	quickSort(array, 0, size - 1, size);
 }
