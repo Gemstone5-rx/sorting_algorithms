f366ceee7947b05d0a5fae08a4d72a0b7537041b
Gemstone5-rx <pearludoka2000@gmail.com>
2023-07-25 13:16:14 -0700

    Updated

--- sort.h
+++ sort.h
@@ -33,6 +33,8 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void merge(size_t lo, size_t mi, size_t hi, int *dest, int *src);
+void merge_partition(size_t lo, size_t hi, int *array, int *base);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
